d0fe73417b869e67b31183359639dd7cb3e260ab
Ignacio Yz <ignacioyz@gmail.com>
2022-06-13 22:43:24 -0400

    commit tree 677a43102838e8bb52894dcfcfb652f52a6d818b

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,22 @@
 // Code EyesOnMe Component Here
+
+
+function EyesOnMe() {
+
+function handleOnFocus(){
+    console.log('Good!')
+}
+
+function handleBlur(){
+    console.log('Hey! Eyes on me!')
+}
+
+return (
+    <button onFocus={handleOnFocus} onBlur={handleBlur}>Eyes on Me</button>
+    
+)
+
+}
+
+
+export default EyesOnMe 
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,14 @@
 // Code Keypad Component Here
+
+function KeyPad() {
+
+function handleChange(){
+    console.log('Entering password...')
+}
+
+    return(
+        <input onChange={handleChange} type="password" />
+    )
+}
+
+export default KeyPad
